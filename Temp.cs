776d80fa0e6f91f740c491e07a5c626a4b44f153
Esther <chisneya96@gmail.com>
2024-02-19 23:25:04 +0100

    program for temperature conversion

---
+++ Temp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class TemperatureConverter
+{
+
+    public static double FahrenheitToCelsius(double fahrenheit)
+    {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    public static double CelsiusToFahrenheit(double celsius)
+    {
+        return celsius * 9 / 5 + 32;
+    }
+
+    public static void Main()
+    {
+        Console.Write("Enter the temperature in Fahrenheit: ");
+        double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+        double celsius = FahrenheitToCelsius(fahrenheit);
+        Console.WriteLine($"{fahrenheit} F = {celsius} C");
+
+        Console.Write("Enter the temperature in Celsius: ");
+        celsius = Convert.ToDouble(Console.ReadLine());
+
+        fahrenheit = CelsiusToFahrenheit(celsius);
+        Console.WriteLine($"{celsius} C = {fahrenheit} F");
+    }
+}
